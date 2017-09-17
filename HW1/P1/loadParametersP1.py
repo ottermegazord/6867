f14aead32c8d8947d86ca447770854a1cc0dea33
ottermegazord <idaly.ali@live.fr>
2017-09-17 16:31:26 -0400

    adds a recursive function that terminates when the difference in objective function value on two succesive step is below convergence threshold assigned.

--- HW1/P1/loadParametersP1.py
+++ HW1/P1/loadParametersP1.py
@@ -6,17 +6,7 @@ import matplotlib as plt
 
 """Data Set Analytical Tools"""
 
-# Crates Gauss and Gradient given gaussMean and gaussCov
-
-# def createGaussAndGradient(gaussMean, gaussCov):
-#     gaussMean = np.mat(gaussMean[:, np.newaxis])
-#     gaussCovInv = np.linalg.inv(gaussCov)
-#     gaussNormalizer = -1/(sqrt(2*pi)**gaussMean.size * np.linalg.det(gaussCov))
-#     def gauss(x):
-#         return gaussNormalizer * exp(-(1/2) *( (x - gaussMean).T * gaussCovInv * (x - gaussMean)))
-#     def gaussGrad(x):
-#         return - gauss(x) * gaussCovInv * (x - gaussMean)
-#     return gauss, gaussGrad
+# Creates gauss and gaussGrad (gradient) given gaussMean and gaussCov
 
 def createGaussAndGradient(gaussMean, gaussCov):
     gaussMean = np.mat(gaussMean[:, np.newaxis]) #
@@ -31,6 +21,18 @@ def createGaussAndGradient(gaussMean, gaussCov):
     def gaussGrad(x):
         return - gauss(x)*gaussCov_Inv*( x - gaussMean)
     return gauss, gaussGrad
+
+# Recursive function that terminates when the difference in objective function value on two successive steps is below
+# convergence threshold assigned
+def convergenceObjectiveFunction(objective_function, convergence_threshold, max_iti):
+    def isConverged(pos, old_pos, iti):
+        if iti > max_iti: return True  # Has already converged
+        objective = objective_function(pos)
+        objective_old_pos = objective_function(old_pos)
+        diff = abs(objective - objective_old_pos)
+        return diff < convergence_threshold
+    return isConverged
+
 
 # Iterative algorithm: in iteration t + 1
 # w^(t+1) = w^t - gradient_t*g(w^t)
@@ -64,5 +66,17 @@ if __name__ == '__main__':
     gaussMean, gaussCov, quadBowlA, quadBowlb = getData()
     print gaussMean, gaussCov, quadBowlA, quadBowlb
 
-    print(createGaussAndGradient(gaussMean, gaussCov))
+    """Define variables"""
+    # Negative gaussian function threshold
+    convCriterionA = 1e-15
+    stepsA = 500
+
+    convCriterionB = 1e-20
+    stepsB = 500
+
+    gauss, gaussGrad = createGaussAndGradient(gaussMean, gaussCov)
+    print(convergenceObjectiveFunction(gauss, convCriterionA, stepsA))
+    print(convergenceObjectiveFunction(gauss, convCriterionB, stepsB))
+
+
 
