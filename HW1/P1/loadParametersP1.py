f08778564961a2d327255d2abcaee186a0c683a8
ottermegazord <idaly.ali@live.fr>
2017-09-17 16:12:40 -0400

    add function that calculates Gauss and Gradient from equations in HW

--- HW1/P1/loadParametersP1.py
+++ HW1/P1/loadParametersP1.py
@@ -1,4 +1,43 @@
 import pylab as pl
+import numpy as np
+from math import *
+import matplotlib as plt
+
+
+"""Data Set Analytical Tools"""
+
+# Crates Gauss and Gradient given gaussMean and gaussCov
+
+# def createGaussAndGradient(gaussMean, gaussCov):
+#     gaussMean = np.mat(gaussMean[:, np.newaxis])
+#     gaussCovInv = np.linalg.inv(gaussCov)
+#     gaussNormalizer = -1/(sqrt(2*pi)**gaussMean.size * np.linalg.det(gaussCov))
+#     def gauss(x):
+#         return gaussNormalizer * exp(-(1/2) *( (x - gaussMean).T * gaussCovInv * (x - gaussMean)))
+#     def gaussGrad(x):
+#         return - gauss(x) * gaussCovInv * (x - gaussMean)
+#     return gauss, gaussGrad
+
+def createGaussAndGradient(gaussMean, gaussCov):
+    gaussMean = np.mat(gaussMean[:, np.newaxis]) #
+    gaussCov_Inv = np.linalg.inv(gaussCov) # Inverses gaussCov
+    n = gaussMean.size
+    determinantGaussCov = np.linalg.det(gaussCov)
+    gaussNormalizer = -1/(sqrt(2*pi)**gaussMean.size * determinantGaussCov)
+
+    # As defined in HW1
+    def gauss(x): # f(x)
+        return gaussNormalizer * exp(-(1/2)*( (x - gaussMean).T * gaussCov_Inv * (x - gaussMean)))
+    def gaussGrad(x):
+        return - gauss(x)*gaussCov_Inv*( x - gaussMean)
+    return gauss, gaussGrad
+
+# Iterative algorithm: in iteration t + 1
+# w^(t+1) = w^t - gradient_t*g(w^t)
+
+def gradientIterative(gradient_t, start, rate):
+    return start - rate * gradient_t(start)
+
 
 def getData():
 
@@ -14,5 +53,16 @@ def getData():
     quadBowlA = data[3:5,:]
     quadBowlb = data[5,:]
 
-    return (gaussMean,gaussCov,quadBowlA,quadBowlb) 
+    return (gaussMean,gaussCov,quadBowlA,quadBowlb)
+
+
+"""Routine starts here"""
+
+if __name__ == '__main__':
+
+    """Load Parameters"""
+    gaussMean, gaussCov, quadBowlA, quadBowlb = getData()
+    print gaussMean, gaussCov, quadBowlA, quadBowlb
+
+    print(createGaussAndGradient(gaussMean, gaussCov))
 
