e55e30baf59f1dd068c7a8ea991514d31df4f82e
ottermegazord <idaly.ali@live.fr>
2017-09-17 17:33:54 -0400

    successfully implemented a gradient descent function for a negative gaussian

--- HW1/P1/loadParametersP1.py
+++ HW1/P1/loadParametersP1.py
@@ -7,7 +7,6 @@ import matplotlib as plt
 """Data Set Analytical Tools"""
 
 # Creates gauss and gaussGrad (gradient) given gaussMean and gaussCov
-
 def createGaussAndGradient(gaussMean, gaussCov):
     gaussMean = np.mat(gaussMean[:, np.newaxis]) #
     gaussCov_Inv = np.linalg.inv(gaussCov) # Inverses gaussCov
@@ -34,9 +33,36 @@ def convergenceObjectiveFunction(objective_function, convergence_threshold, max_
     return isConverged
 
 
+# # Outputs position of local mininum
+def basicGradientDescent(gradient_function, start_pos, rate, isConverged, output):
+    if isinstance(rate, float):
+        frate = lambda x: rate
+    else:
+        frate = rate
+    old_pos = None
+    pos = start_pos
+    iti = 0
+    while old_pos is None or not isConverged(pos, old_pos, iti):
+        old_pos = pos # updates new position
+        iti = iti + 1 # adds 1 to current iti
+        pos = gradientIterative(gradient_function, pos, frate(iti))
+        output.append((pos, iti))
+        if iti % 10 == 0:
+            pass
+    return pos
+
+
+# # Recursive function that terminates when the difference in norm on two successive steps is below
+# # convergence threshold assigned
+# def covergenceGradientFunction(gradient_function, convergence_threshold, max_iti):
+#     def isConverged(pos, old_pos, iti):
+#         if iti > max_iti: return True # has already converged
+#         return np.linalg.norm(gradient_function(pos)) < convergence_threshold
+#     return isConverged
+
+
 # Iterative algorithm: in iteration t + 1
 # w^(t+1) = w^t - gradient_t*g(w^t)
-
 def gradientIterative(gradient_t, start, rate):
     return start - rate * gradient_t(start)
 
@@ -68,15 +94,22 @@ if __name__ == '__main__':
 
     """Define variables"""
     # Negative gaussian function threshold
-    convCriterionA = 1e-15
-    stepsA = 500
+    convCriterionA = 1e-20
+    stepsA = 1000
 
     convCriterionB = 1e-20
-    stepsB = 500
+    stepsB = 1000
+
+    """Output of Gradient Descent"""
+    output = [] #local minimum stores here
+    start_matrix = np.mat([[0.], [0.]])
 
     gauss, gaussGrad = createGaussAndGradient(gaussMean, gaussCov)
-    print(convergenceObjectiveFunction(gauss, convCriterionA, stepsA))
-    print(convergenceObjectiveFunction(gauss, convCriterionB, stepsB))
+    convergedO = convergenceObjectiveFunction(gauss, convCriterionA, stepsA)
+    print(convergedO)
+    print(basicGradientDescent(gaussGrad, start_matrix, 1e8, convergedO, output))
+
+
 
 
 
